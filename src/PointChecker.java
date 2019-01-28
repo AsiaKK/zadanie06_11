fe8ba462c4d4fdcaef7531cb0260f370ad78b596
AsiaKK <joanna.kaminska83@gmail.com>
2019-01-28 14:51:45 +0100

    commit after fix

--- src/PointChecker.java
+++ src/PointChecker.java
@@ -4,7 +4,9 @@ public class PointChecker {
         int x = inputPoint.getX();
         int y = inputPoint.getY();
 
-        if (x >= 0 && y >= 0) {
+        if (x==0 && y==0){
+            System.out.println("To jest środek układu współrzędnych.");
+        }else if (x >= 0 && y >= 0) {
             System.out.println("Punkt znajduje się w pierwszej ćwiartce.");
         } else if (x < 0 && y >= 0) {
             System.out.println("Punkt znajduje się w drugiej ćwiartce.");
